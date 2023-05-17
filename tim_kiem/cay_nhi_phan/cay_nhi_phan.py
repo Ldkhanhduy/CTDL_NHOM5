38a94c9934183a8de9ce6d1574a2bcdb93c907a3
Ldkhanhduy <90101790+Ldkhanhduy@users.noreply.github.com>
2023-05-17 15:49:04 +0700

    Update cay_nhi_phan.py

--- tim_kiem/cay_nhi_phan/cay_nhi_phan.py
+++ tim_kiem/cay_nhi_phan/cay_nhi_phan.py
@@ -1,23 +1,23 @@
-class Cay:
+class Cay: #Tạo cây
     def __init__(self, data):
-        self.data = data
-        self.trai = None
-        self.phai = None
+        self.data = data #dữ liệu của khóa
+        self.trai = None #con trỏ trái
+        self.phai = None #con trỏ phải
 
-def tim(so, root,i):
+def tim(so, root,i): #Tìm kiếm một số trong cây
     if (root):
         if so == root.data:
-            print("Nằm ở bậc", i)
+            print("Nằm ở bậc", i) #Số cần tìm nằm ở gốc
         else:
-            if so < root.data:
+            if so < root.data: #số cần tìm nhỏ hơn khóa tại con trỏ đang đứng
                 i += 1
                 print("Trái")
                 tim(so, root.trai,i)
-            else:
+            else: #số cần tìm lớn hơn khóa tại con trỏ đang đứng
                 i += 1
                 print("Phải")
                 tim(so, root.phai,i)
-    else: print("Không tìm thấy")
+    else: print("Không tìm thấy") #số cần tìm không có trong cây
 
 if __name__ == '__main__':
     print("Cây nhị phân với khóa là số nguyên")
@@ -34,4 +34,4 @@ if __name__ == '__main__':
     root.trai.phai.trai = Cay(30)
     root.trai.phai.trai.phai = Cay(40)
     root.trai.phai.trai.trai = Cay(27)
-    tim(x, root, 1)
+    tim(x, root, 1) #khi tìm sẽ in ra màn hình đường đi của thuật toán và chỉ ra tầng của số cần tìm
