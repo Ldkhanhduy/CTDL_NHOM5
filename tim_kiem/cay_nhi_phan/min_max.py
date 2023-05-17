9b158970d318ca4859b2864b2e76610f62cb6ea1
Ldkhanhduy <90101790+Ldkhanhduy@users.noreply.github.com>
2023-05-17 15:55:50 +0700

    Update and rename min.py to min_max.py

--- tim_kiem/cay_nhi_phan/min_max.py
+++ tim_kiem/cay_nhi_phan/min_max.py
@@ -6,6 +6,12 @@ def min(root):
             print("Giá trị nhỏ nhất là:", root.data)
         else: min(root.trai)
 
+def max(root):
+    if (root):
+        if root.phai == None:
+            print("Giá trị lớn nhất là:", root.data)
+        else: max(root.phai)
+
 if __name__ == '__main__':
     root = Cay(69)
     root.trai = Cay(24)
@@ -19,4 +25,8 @@ if __name__ == '__main__':
     root.trai.phai.trai = Cay(30)
     root.trai.phai.trai.phai = Cay(40)
     root.trai.phai.trai.trai = Cay(27)
-    min(root)
+    x = input("Nhập 1 nếu tìm GTLN, 2 nếu tìm GTNN:")
+    if x ==1:
+        max(root)
+    else:
+        min(root)
